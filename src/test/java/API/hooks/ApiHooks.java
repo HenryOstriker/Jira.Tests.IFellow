f80f14146aeac8737e410c1d66874fcd4d4b674f
Henry <ostriker99@mail.ru>
2023-10-03 17:36:52 +0300

    Commit1

---
+++ src/test/java/API/hooks/ApiHooks.java
@@ -0,0 +1,37 @@
+package API.hooks;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import static utils.Configuration.getConfigurationValue;
+
+public abstract class ApiHooks {
+    public static RequestSpecification rickAndMorty;
+    public static RequestSpecification reqRes;
+
+    @BeforeAll
+    static void setUp() {
+         rickAndMorty = new RequestSpecBuilder()
+                 .setBaseUri(getConfigurationValue("rickAndMortyUrl"))
+                 .setContentType(ContentType.JSON)
+                 .setAccept(ContentType.JSON)
+                 .addFilter(new AllureRestAssured())
+                 .addFilter(new RequestLoggingFilter())
+                 .addFilter(new ResponseLoggingFilter())
+                 .build();
+
+         reqRes = new RequestSpecBuilder()
+                 .setBaseUri(getConfigurationValue("reqResUrl"))
+                 .setContentType(ContentType.JSON)
+                 .setAccept(ContentType.JSON)
+                 .addFilter(new AllureRestAssured())
+                 .addFilter(new RequestLoggingFilter())
+                 .addFilter(new ResponseLoggingFilter())
+                 .build();
+    }
+}
